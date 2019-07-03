8695c8cd91b3540547cd764abc534429aecb3d72
victor <v.a.kozlov@tmn3.etagi.com>
2019-07-03 15:37:34 +0500

    complete task Date.format

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,9 +1,12 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
-fun Date.format(pattern: String) {
-    TODO("not implemented")
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy") : String{
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(dateFormat)
 }
 
 fun Date.add(value: Int, timeUnit: TimeUnits) {
