3fd9f9b3e7da926afe9da398dde62156c8315859
KozlovVictor <KozlovVictor@users.noreply.github.com>
2019-07-10 00:50:57 +0500

    task *String.stripHtml in progress

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -8,6 +8,45 @@ fun String.truncate(count: Int = 16): String {
     ) else tempString}..."
 }
 
-fun String.stripHtml() {
- //TODO
+fun String.stripHtml(): String {
+    var isTag = false
+    var isEscape = false
+    var spaceCount = 0
+    val startTag = "<"
+    val endTag = ">"
+    val startEscape = "&"
+    val endEscape = ";"
+    val result = StringBuilder()
+    this.forEach { char ->
+        if (!isTag) {
+            if (!isEscape) {
+                if (char.toString() != startTag) {
+                    if (char.toString() != startEscape) {
+                        if (char.toString() != " ") {
+                            if (char.toByte().toInt() !in 16..79) {
+                                spaceCount = 0
+                                result.append(char.toString())
+                            }
+                        } else {
+                            if (spaceCount < 1) {
+                                spaceCount++
+                                result.append(char.toString())
+                            } else {
+                                spaceCount++
+                            }
+                        }
+                    } else {
+                        isEscape = true
+                    }
+                } else {
+                    isTag = true
+                }
+            } else {
+                if (char.toString() == endEscape) isEscape = false
+            }
+        } else {
+            if (char.toString() == endTag) isTag = false
+        }
+    }
+    return result.toString()
 }
--- app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -158,4 +158,41 @@ class ExampleUnitTest {
         println("Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15)) //Bender Bending...
         println("A     ".truncate(3)) //A
     }
+
+    @Test
+    fun test_stripHtml() {
+        println("&игра; amp lt &gt 39; meters ()quot;".stripHtml())
+//        println('А'.toByte().toInt())
+//        println('Б'.toByte().toInt())
+//        println('Ю'.toByte().toInt())
+//        println('Я'.toByte().toInt())
+//        println("--------------------")
+//        println('а'.toByte().toInt())
+//        println('б'.toByte().toInt())
+//        println('я'.toByte().toInt())
+
+        assertEquals("Образовательное IT-сообщество Skill Branch",
+            "<p class=\"title\">Образовательное IT-сообщество Skill Branch</p>".stripHtml())
+        assertEquals("Образовательное IT-сообщество Skill Branch",
+            "<p>Образовательное       IT-сообщество Skill Branch</p>".stripHtml())
+
+        /* additional tests */
+        assertEquals("single", "&amp;&lt;&gt;single&#39;&quot;".stripHtml())
+        assertEquals("", "&amp;&lt;&gt;&#39;&quot;".stripHtml())
+        assertEquals(" ", "&amp;&lt;&gt;    &#39;&quot;".stripHtml())
+        assertEquals("1978", "<path fill=\"Color\" d=\"M11.63 10z\"></svg><span>1978</span>".stripHtml())
+        assertEquals("", "&gt;<head>&#39;&quot;</head>".stripHtml())
+        assertEquals(" ", "&gt;<head> &quot; </head>".stripHtml())
+        assertEquals("&игра; amp lt &gt 39; meters ()quot;", "&игра; amp lt &gt 39; meters ()quot;".stripHtml())
+        assertEquals(" one two ", "  one   two ".stripHtml())
+        assertEquals("null", "null".stripHtml())
+        val longHtml = """
+            <TD valign="top" style="padding-bottom:15px;"> <b>line1<b> </TD>
+            <TD valign="top"> <span class="HeadTitleNews"> line2</span>
+            <img src='http://2011WaterpoloF.jpg' >
+            <div style="margin: 0in 0in 0pt">line3</div>
+        """.trimIndent()
+        println(longHtml.stripHtml())
+        assertEquals(" line1 \n line2\n\nline3", longHtml.stripHtml())
+    }
 }
