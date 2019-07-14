2af906e94d1f791be56986ab6f0fe23143f1d210
KozlovVictor <KozlovVictor@users.noreply.github.com>
2019-07-14 19:44:11 +0500

    init hometask_3

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,7 +1,7 @@
 package ru.skillbranch.devintensive
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
