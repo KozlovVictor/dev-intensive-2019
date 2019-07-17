8971e43dcfd7dc84d4748130a41489c2133189b0
KozlovVictor <KozlovVictor@users.noreply.github.com>
2019-07-18 00:04:12 +0500

    complete tasks Bender.listenAnswer (positive case) and Bender.listenAnswer (negative case)

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -13,7 +13,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     }
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
-        return if (question.answers.contains(answer.toLowerCase() && validation(answer))) { //  && validation(answer)
+        return if (question.answers.contains(answer.toLowerCase()) && validation(answer)) { //  && validation(answer)
             wrongAnswerCount = 0
             question = question.nextQuestion()
             "Отлично - ты справился\n${question.question}" to status.color
